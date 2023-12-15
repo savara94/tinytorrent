4eb40ee0e1e5e23a6ebb361a15ab76520ceba85a
Stefan Savic <stefan.savic@doob3dlabs.com>
2023-12-15 14:53:10 +0100

    tests refactored

--- bencode/bencode_test.go
+++ bencode/bencode_test.go
@@ -1,6 +1,7 @@
 package bencode
 
 import (
+	"encoding/json"
 	"io"
 	"reflect"
 	"strings"
@@ -8,215 +9,201 @@ import (
 )
 
 type testCase struct {
-	reader    io.Reader
+	name      string
+	input     string
 	want      any
 	wantedErr error
 }
 
-type testCmpFn func(got, want any) bool
+type decoderAsserterFn func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error)
 
-func simpleCmp(got, want any) bool {
-	return got == want
-}
+func noDecoderTypeCheckingAssert(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+	t.Helper()
 
-func sliceCmp(got, want any) bool {
-	return reflect.DeepEqual(got, want)
+	var got any
+	want := testCase.want
+	name := testCase.name
+
+	err := decoder.Decode(&got)
+
+	if err == nil {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("name %s: got %v, want %v", name, got, want)
+		}
+	}
+
+	return got, err
 }
 
 func TestDecode(t *testing.T) {
+	testGeneral(t)
+	testNumbers(t)
+	testStrings(t)
+	testLists(t)
+	testDicts(t)
+}
+
+func testGeneral(t *testing.T) {
 	generalTestCases := []testCase{
-		// No valid delimiter
-		{strings.NewReader("psa"), nil, io.EOF},
+		{"No valid delimiter", "psa", nil, io.EOF},
 	}
 
+	runTestCases(generalTestCases, noDecoderTypeCheckingAssert, t)
+}
+
+func testNumbers(t *testing.T) {
 	numberTestCases := []testCase{
-		// Good weather cases
-		{strings.NewReader("i45e"), 45, nil},
-		{strings.NewReader("i567e"), 567, nil},
-		{strings.NewReader("i-45e"), -45, nil},
-		// Missing number
-		{strings.NewReader("ie"), nil, ErrExpectedNumber},
-		// Missing 'e'
-		{strings.NewReader("i456"), nil, io.EOF},
+		{"1st number", "i45e", 45, nil},
+		{"2nd number", "i567e", 567, nil},
+		{"3rd number", "i-45e", -45, nil},
+		{"Missing number case", "ie", nil, ErrExpectedNumber},
+		{"Missing number terminator", "i456", nil, io.EOF},
 	}
 
-	stringParsingTestCases := []testCase{
-		// Good weather case
-		{strings.NewReader("3:ben"), "ben", nil},
+	runTestCases(numberTestCases, noDecoderTypeCheckingAssert, t)
+
+	intTypeCheckingAssert := func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+		var got int
+
+		err := decoder.Decode(&got)
+
+		if err == nil {
+			if got != testCase.want {
+				t.Errorf("%s got %d want %d", testCase.name, got, testCase.want)
+			}
+		}
+
+		return got, err
+	}
+
+	runTestCases(numberTestCases, intTypeCheckingAssert, t)
+}
+
+func testStrings(t *testing.T) {
+	stringTestCases := []testCase{
+		{"1st string", "3:ben", "ben", nil},
 		// Less characters than specified
-		{strings.NewReader("4:ben"), nil, ErrEndOfReader},
+		{"2nd string", "4:ben", nil, ErrEndOfReader},
 		// More characters than specified
-		{strings.NewReader("2:ben"), "be", nil},
+		{"3rd string", "2:ben", "be", nil},
 		// 0 characters specified
-		{strings.NewReader("0:"), "", nil},
-		// 1 character specified
-		{strings.NewReader("1:a"), "a", nil},
+		{"4th string", "0:", "", nil},
+		{"5th string", "1:a", "a", nil},
 	}
 
-	listParsingTestCases := []testCase{
-		// Good weather cases
-		{strings.NewReader("l3:ben2:goe"), []any{"ben", "go"}, nil},
-		{strings.NewReader("l3:beni56ee"), []any{"ben", 56}, nil},
-		// Empty list
-		{strings.NewReader("le"), []any{}, nil},
-		// Missing end of list
-		{strings.NewReader("l"), nil, io.EOF},
+	runTestCases(stringTestCases, noDecoderTypeCheckingAssert, t)
+
+	stringTypeCheck := func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+		var got string
+
+		err := decoder.Decode(&got)
+
+		if err == nil {
+			if got != testCase.want {
+				t.Errorf("%s got %s want %s", testCase.name, got, testCase.want)
+			}
+		}
+
+		return got, err
 	}
 
-	dictParsingTestCases := []testCase{
-		// Good weather cases
-		{strings.NewReader("d3:ben2:goe"), map[string]any{"ben": "go"}, nil},
-		{strings.NewReader("d3:beni56ee"), map[string]any{"ben": 56}, nil},
-		// Empty dict
-		{strings.NewReader("de"), map[string]any{}, nil},
-		// Missing end of dict
-		{strings.NewReader("d"), nil, io.EOF},
+	runTestCases(stringTestCases, stringTypeCheck, t)
+}
+
+func testLists(t *testing.T) {
+	listTestCases := []testCase{
+		{"1st list", "l3:ben2:goe", []any{"ben", "go"}, nil},
+		{"2nd list", "l3:beni56ee", []any{"ben", 56}, nil},
+		{"Empty list", "le", []any{}, nil},
+		{"Nested lists", "llelleei0ee", []any{[]any{}, []any{[]any{}}, 0}, nil},
+		{"Missing end of list", "l", nil, io.EOF},
 	}
 
-	runTestCases("general", generalTestCases, t, simpleCmp)
-	runTestCases("numbers", numberTestCases, t, simpleCmp)
-	runTestCases("strings", stringParsingTestCases, t, simpleCmp)
-	runTestCases("lists", listParsingTestCases, t, sliceCmp)
-	runTestCases("dicts", dictParsingTestCases, t, sliceCmp)
+	runTestCases(listTestCases, noDecoderTypeCheckingAssert, t)
 
-	complexTestCases := []testCase{
-		// Nested lists
-		{strings.NewReader("llelleei0ee"), []any{[]any{}, []any{[]any{}}, 0}, nil},
-		// Nested dicts
-		{strings.NewReader("d3:bend3:ben3:kenee"), map[string]any{"ben": map[string]any{"ben": "ken"}}, nil},
+	intListTypeCheckingCases := []testCase{
+		{"int list", "li1ei2ei3ee", []int{1, 2, 3}, nil},
 	}
 
-	runTestCases("complex", complexTestCases, t, sliceCmp)
+	intListTypeCheckingAssert := func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+		var got []int
+
+		err := decoder.Decode(&got)
+
+		if err == nil {
+			if !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("got %v wanted %v", got, testCase.want)
+			}
+		}
+
+		return got, err
+	}
+
+	runTestCases(intListTypeCheckingCases, intListTypeCheckingAssert, t)
+
+	stringListTypeCheckingCases := []testCase{
+		{"string list", "l3:ben3:ken4:gwene", []string{"ben", "ken", "gwen"}, nil},
+	}
+
+	stringListTypeCheckingAssert := func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+		var got []string
+
+		err := decoder.Decode(&got)
+
+		if err == nil {
+			if !reflect.DeepEqual(got, testCase.want) {
+				t.Errorf("got %v wanted %v", got, testCase.want)
+			}
+		}
+
+		return got, err
+	}
+
+	runTestCases(stringListTypeCheckingCases, stringListTypeCheckingAssert, t)
+}
+
+func testDicts(t *testing.T) {
+	dictTestCases := []testCase{
+		{"1st dict", "d3:ben2:goe", map[string]any{"ben": "go"}, nil},
+		{"2nd dict", "d3:beni56ee", map[string]any{"ben": 56}, nil},
+		{"Empty dict", "de", map[string]any{}, nil},
+		{"Nested dicts", "d3:bend3:ben3:kenee", map[string]any{"ben": map[string]any{"ben": "ken"}}, nil},
+		{"Missing dict terminator", "d", nil, io.EOF},
+	}
+
+	runTestCases(dictTestCases, noDecoderTypeCheckingAssert, t)
+
+	type MatchingStructExample struct {
+		Ben      string `bencode:"ben"`
+		Number   int    `bencode:"number"`
+		List     []int  `bencode:"list"`
+		Nullable *int   `bencode:"nullable"`
+	}
+
+	structAssignmentCases := []testCase{
+		{"1st struct", "d3:ben3:ken6:numberi3e4:listli1ei2ei3ee6:nestedd3:key5:valueee", MatchingStructExample{"ken", 3, []int{1, 2, 3}, nil}, nil},
+		{"2nd struct", "d3:ben3:ken6:numberi3e4:listli1ei2ei3ee6:nestedd3:key5:valuee8:nullablei5ee", MatchingStructExample{"ken", 3, []int{1, 2, 3}, new(int)}, nil},
+	}
+
+	runTestCases(structAssignmentCases, func(decoder *Decoder, testCase *testCase, t *testing.T) (any, error) {
+		var got MatchingStructExample
+
+		err := decoder.Decode(&got)
+
+		if err == nil {
+			gotJson, _ := json.Marshal(got)
+			wantJson, _ := json.Marshal(testCase.want)
+
+			if !reflect.DeepEqual(gotJson, wantJson) {
+				t.Errorf("%s got %v want %v", testCase.name, string(gotJson), string(wantJson))
+			}
+		}
+
+		return got, err
+	}, t)
 }
 
 // func TestUnmarshal(t *testing.T) {
-// 	intTestCases := []struct {
-// 		input   any
-// 		want    int
-// 		wantErr bool
-// 	}{
-// 		{123, 123, false},
-// 		{-123, -123, false},
-// 		{"pwd", 0, true},
-// 		{make([]any, 1), 0, true},
-// 		{make(map[string]any), 0, true},
-// 	}
-
-// 	for i := range intTestCases {
-// 		input := intTestCases[i].input
-// 		wantErr := intTestCases[i].wantErr
-
-// 		var a int
-
-// 		err := Unmarshal(input, &a)
-
-// 		if err == nil && !wantErr {
-// 			if input != a {
-// 				t.Errorf("wanted %v, got %v", input, a)
-// 			}
-// 		}
-
-// 		if err != nil && !wantErr {
-// 			t.Errorf("did not expect error %v", err)
-// 		}
-// 	}
-
-// 	stringTestCases := []struct {
-// 		input   any
-// 		wantErr bool
-// 	}{
-// 		{"123", false},
-// 		{-123, true},
-// 		{"pwd", false},
-// 		{make([]any, 1), true},
-// 		{make(map[string]any), true},
-// 	}
-
-// 	for i := range stringTestCases {
-// 		input := stringTestCases[i].input
-// 		want := stringTestCases[i].input
-// 		wantErr := stringTestCases[i].wantErr
-
-// 		var a string
-
-// 		err := Unmarshal(input, &a)
-
-// 		if err == nil && !wantErr {
-// 			if want != a {
-// 				t.Errorf("wanted %v, got %v", want, a)
-// 			}
-// 		}
-
-// 		if err != nil && !wantErr {
-// 			t.Errorf("did not expect error %v", err)
-// 		}
-// 	}
-
-// 	listInt := []int{1, 2, 3}
-// 	var targetListInt []int
-
-// 	err := Unmarshal(toSliceOfAny(listInt), &targetListInt)
-// 	if err != nil {
-// 		t.Errorf("Not expected error %v", err)
-// 	} else {
-// 		if !reflect.DeepEqual(listInt, targetListInt) {
-// 			t.Errorf("Not equal %v and %v", listInt, targetListInt)
-// 		}
-// 	}
-
-// 	listString := []string{"a", "b", "c"}
-// 	var targetListString []string
-
-// 	err = Unmarshal(toSliceOfAny(listString), &targetListString)
-// 	if err != nil {
-// 		t.Errorf("Not expected error %v", err)
-// 	} else {
-// 		if !reflect.DeepEqual(listString, targetListString) {
-// 			t.Errorf("Not equal %v and %v", listString, targetListString)
-// 		}
-// 	}
-
-// 	dict := map[string]any{
-// 		"ben":    "ken",
-// 		"number": 3,
-// 		"list":   []any{1, 2, 3},
-// 		"nested": map[string]any{"key": "value"},
-// 	}
-
-// 	goodStruct := struct {
-// 		Ben      string `bencode:"ben"`
-// 		Number   int    `bencode:"number"`
-// 		List     []int  `bencode:"list"`
-// 		Nullable *int   `bencode:"nullable"`
-// 	}{}
-
-// 	err = Unmarshal(dict, &goodStruct)
-// 	if err != nil {
-// 		t.Errorf("Not expected error %v", err)
-// 	}
-
-// 	if goodStruct.Ben != dict["ben"] || goodStruct.Number != dict["number"] || !reflect.DeepEqual(goodStruct.List, []int{1, 2, 3}) {
-// 		t.Errorf("Not equal %v", goodStruct)
-// 	}
-
-// 	dict["nullable"] = 5
-// 	err = Unmarshal(dict, &goodStruct)
-// 	if err != nil {
-// 		t.Errorf("Not expected error %v", err)
-// 	}
-
-// 	if *goodStruct.Nullable != dict["nullable"] {
-// 		t.Errorf("Not equal %v", goodStruct)
-// 	}
-// }
-
-// func toSliceOfAny[T any](s []T) []any {
-// 	result := make([]any, len(s))
-// 	for i, v := range s {
-// 		result[i] = v
-// 	}
-// 	return result
-// }
 
 // func TestEncode(t *testing.T) {
 // 	testCases := []struct {
@@ -260,38 +247,29 @@ func TestDecode(t *testing.T) {
 // 	}
 // }
 
-func runTestCases(category string, testCases []testCase, t *testing.T, cmpFn testCmpFn) {
+func runTestCases(testCases []testCase, assertDecoder decoderAsserterFn, t *testing.T) {
 	for i := range testCases {
 		testCase := testCases[i]
 
-		reader := testCase.reader
-		want := testCase.want
+		reader := strings.NewReader(testCase.input)
 		wantedErr := testCase.wantedErr
+		name := testCase.name
 
 		decoder := NewDecoder(reader)
 
-		var got any
-
-		err := decoder.Decode(&got)
-
-		assertError(category, i, wantedErr, err, t)
-
-		if err == nil {
-			if !cmpFn(got, want) {
-				t.Errorf("category %s case %d: got %v, want %v", category, i, got, want)
-			}
-		}
+		_, err := assertDecoder(decoder, &testCase, t)
+		assertError(name, wantedErr, err, t)
 	}
 }
 
-func assertError(category string, i int, wantedError, gottenError error, t *testing.T) {
+func assertError(name string, wantedError, gottenError error, t *testing.T) {
 	t.Helper()
 
 	if wantedError != nil && gottenError != wantedError {
-		t.Errorf("category %s case %d: got %v, wanted %v", category, i, gottenError, wantedError)
+		t.Errorf("name %s: got %v, wanted %v", name, gottenError, wantedError)
 	}
 
 	if wantedError == nil && gottenError != nil {
-		t.Errorf("category %s case %d: got %v wanted nil", category, i, gottenError)
+		t.Errorf("name %s: got %v wanted nil", name, gottenError)
 	}
 }
