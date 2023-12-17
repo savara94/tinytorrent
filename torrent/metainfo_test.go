f92f8a09ec9d73f568c44af0f0ccc35864d425fa
Stefan Savic <stefan.savic@doob3dlabs.com>
2023-12-17 23:19:37 +0100

    Tighten up metainfo_test

--- torrent/metainfo_test.go
+++ torrent/metainfo_test.go
@@ -6,80 +6,75 @@ import (
 	"testing"
 )
 
-type metaInfoWant struct {
+type metaInfoTestCase struct {
 	announce string
 	name     string
+	infoHash string
 }
 
-type testCaseMetaInfo struct {
+type testCase struct {
+	name      string
 	filePath  string
-	want      *metaInfoWant
+	want      metaInfoTestCase
 	wantedErr error
-	infoHash  string
 }
 
-func TestParseMetaInfo(t *testing.T) {
-	testCases := []testCaseMetaInfo{
-		{"examples/hello_world.torrent", &metaInfoWant{"http://localhost:6969/announce", "hello_world"}, nil, "2af633c618e64c9ea3972789f5764fbea6f42d40"},
-		{"examples/lovecraft.torrent", &metaInfoWant{"http://localhost:6969/announce", "sevenhplovecraftstories_pc_librivox"}, nil, ""},
-		{"examples/missing_length_and_files.torrent", nil, ErrLengthAndFilesNotSpecified, ""},
+func assertMetaInfo(testCase *testCase, t *testing.T) error {
+	name := testCase.name
+	filePath := testCase.filePath
+
+	reader, err := os.Open(filePath)
+	if err != nil {
+		return err
 	}
 
-	for i := range testCases {
-		testCase := testCases[i]
-		filePath := testCase.filePath
-		wantedErr := testCase.wantedErr
-		want := testCase.want
-		wantedInfoHash := testCase.infoHash
+	defer reader.Close()
 
-		reader, err := os.Open(filePath)
-		defer reader.Close()
+	info, err := ParseMetaInfo(reader)
+	if err != nil {
+		return err
+	}
 
-		if err != nil {
-			t.Errorf("%d Failed opening %s", i, filePath)
-		}
+	metaInfoGot := metaInfoTestCase{name: info.Info.Name, announce: info.Announce, infoHash: infoHashToHexString(info)}
 
-		gotInfo, gottenErr := ParseMetaInfo(reader)
+	if testCase.want != metaInfoGot {
+		t.Errorf("%s got %#v wanted %#v", name, metaInfoGot, testCase.want)
+	}
 
-		if wantedErr == nil && gottenErr != nil {
-			t.Errorf("%d Got error %v, wasn't expecting one", i, gottenErr)
-		}
+	return nil
+}
 
-		if wantedErr != nil && gottenErr == nil {
-			t.Errorf("wanted %v got nil", wantedErr)
-		}
+func assertError(err error, testCase *testCase, t *testing.T) {
+	name := testCase.name
+	wantedErr := testCase.wantedErr
 
-		if wantedErr != nil && gottenErr != nil {
-			if wantedErr != gottenErr {
-				t.Errorf("wanted %v got %v", wantedErr, gottenErr)
-			}
-		}
+	if wantedErr == nil && err != nil {
+		t.Errorf("%s Got error %v, wasn't expecting one", name, err)
+	}
 
-		if want != nil && gottenErr == nil {
-			if want.name != gotInfo.Info.Name {
-				t.Errorf("wanted %s got %s", want.name, gotInfo.Info.Name)
-			}
+	if wantedErr != nil && err == nil {
+		t.Errorf("%s wanted %v got nil", name, wantedErr)
+	}
 
-			if wantedInfoHash != "" {
-				infoHashHexString := infoHashToHexString(gotInfo)
-				if wantedInfoHash != infoHashHexString {
-					t.Errorf("wanted %s got %s", wantedInfoHash, infoHashHexString)
-				}
-			}
-
-			bencoded, err := gotInfo.Encode()
-
-			if err != nil {
-				t.Errorf("%v", err)
-			}
-			os.WriteFile(filePath+"_test.torrent", bencoded, 0644)
-			t.Logf("%d", len(gotInfo.Info.Pieces))
-		}
-
+	if wantedErr != err {
+		t.Errorf("%s wanted %v got %v", name, wantedErr, err)
 	}
 }
 
 func infoHashToHexString(metaInfo *MetaInfo) string {
 	infoHash := metaInfo.GetInfoHash()
 	return hex.EncodeToString(infoHash)
+}
+
+func TestParseMetaInfo(t *testing.T) {
+	testCases := []testCase{
+		{"hello world", "examples/hello_world.torrent", metaInfoTestCase{"http://localhost:6969/announce", "hello_world", "2af633c618e64c9ea3972789f5764fbea6f42d40"}, nil},
+		{"lovecraft", "examples/lovecraft.torrent", metaInfoTestCase{"http://bt1.archive.org:6969/announce", "sevenhplovecraftstories_pc_librivox", "588fb9c2cf9f7ceb973976c1e0eaaf38c3444999"}, nil},
+		{"missing length and files", "examples/missing_length_and_files.torrent", metaInfoTestCase{}, ErrLengthAndFilesNotSpecified},
+	}
+
+	for i := range testCases {
+		err := assertMetaInfo(&testCases[i], t)
+		assertError(err, &testCases[i], t)
+	}
 }
