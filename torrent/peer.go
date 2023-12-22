3c2fd1588fae9fe0c0b6224f9bfc2ad56a683a7d
Stefan Savic <stefan.savic@doob3dlabs.com>
2023-12-23 00:04:57 +0100

    Initiate Handshake

--- torrent/peer.go
+++ torrent/peer.go
@@ -1,5 +1,10 @@
 package torrent
 
+import (
+	"errors"
+	"io"
+)
+
 type Peer struct {
 	PeerId       []byte
 	Port         int
@@ -56,4 +61,55 @@ type PiecePayload struct {
 	Index int32
 	Begin int32
 	Piece []byte
+}
+
+type Seeder struct {
+	SeederInfo   PeerInfo
+	SeederWriter io.Writer
+	SeederReader io.Reader
+	MetaInfo     *MetaInfo
+}
+
+func (seeder *Seeder) InitiateHandshake() error {
+	n, err := seeder.SeederWriter.Write([]byte(HandshakeMsg))
+	if err != nil {
+		return err
+	}
+
+	if n < len(HandshakeMsg) {
+		return errors.New("Couldn't send handshake bytes")
+	}
+
+	// 8 empty bytes are next
+	reservedBytes := []byte{0, 0, 0, 0, 0, 0, 0, 0}
+	n, err = seeder.SeederWriter.Write(reservedBytes)
+	if err != nil {
+		return err
+	}
+
+	if n < len(reservedBytes) {
+		return errors.New("Couldn't send reserved bytes.")
+	}
+
+	infoHash := seeder.MetaInfo.GetInfoHash()
+	n, err = seeder.SeederWriter.Write(infoHash)
+	if err != nil {
+		return err
+	}
+
+	if n < len(infoHash) {
+		return errors.New("Couldn't send infohash bytes.")
+	}
+
+	n, err = seeder.SeederWriter.Write(seeder.SeederInfo.PeerId)
+	if err != nil {
+		return err
+	}
+
+	if n < len(seeder.SeederInfo.PeerId) {
+		return errors.New("Couldn't send peer id bytes.")
+	}
+
+	// If seeder agrees, it won't severe the connection.
+	return nil
 }
