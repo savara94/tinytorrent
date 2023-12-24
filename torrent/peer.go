a7b42a2cf8b161a188e98788330193be0076cb86
Stefan Savić <stephon.savic@gmail.com>
2023-12-24 21:23:18 +0100

    Peer (#11)
    * Added message structs
    
    * Initiate Handshake
    
    * Added send/receive peer messages
    
    * Send tests
    
    * Added receive tests
    
    * Added handshake tests
    
    ---------
    
    Co-authored-by: Stefan Savic <stefan.savic@doob3dlabs.com>

--- torrent/peer.go
+++ torrent/peer.go
@@ -1,7 +1,221 @@
 package torrent
 
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+)
+
 type Peer struct {
 	PeerId       []byte
 	Port         int
 	MetaInfoList []MetaInfo
+}
+
+const HandshakeMsg = "\x19" + "BitTorrent protocol"
+
+const (
+	Choke byte = iota
+	Unchoke
+	Interested
+	NotInterested
+	Have
+	Bitfield
+	Request
+	Piece
+	Cancel
+	KeepAlive
+)
+
+type PeerMessage struct {
+	Type    byte
+	Payload any
+}
+
+var ChokeMessage = PeerMessage{Type: Choke, Payload: nil}
+var UnchokeMessage = PeerMessage{Type: Unchoke, Payload: nil}
+var InterestedMessage = PeerMessage{Type: Interested, Payload: nil}
+var NotInterestedMessage = PeerMessage{Type: NotInterested, Payload: nil}
+var KeepAliveMessage = PeerMessage{Type: KeepAlive, Payload: nil}
+
+type BitfieldPayload struct {
+	Bitfield []byte
+}
+
+type HavePayload struct {
+	Index int32
+}
+
+type RequestPayload struct {
+	Index  int32
+	Begin  int32
+	Length int32
+}
+
+type CancelPayload struct {
+	Index  int32
+	Begin  int32
+	Length int32
+}
+
+type PiecePayload struct {
+	Index int32
+	Begin int32
+	Piece []byte
+}
+
+type Seeder struct {
+	SeederInfo   PeerInfo
+	SeederWriter io.Writer
+	SeederReader io.Reader
+	MetaInfo     *MetaInfo
+}
+
+func (seeder *Seeder) InitiateHandshake() error {
+	n, err := seeder.SeederWriter.Write([]byte(HandshakeMsg))
+	if err != nil {
+		return err
+	}
+
+	if n < len(HandshakeMsg) {
+		return errors.New("Couldn't send handshake bytes")
+	}
+
+	// 8 empty bytes are next
+	reservedBytes := []byte{0, 0, 0, 0, 0, 0, 0, 0}
+	n, err = seeder.SeederWriter.Write(reservedBytes)
+	if err != nil {
+		return err
+	}
+
+	if n < len(reservedBytes) {
+		return errors.New("Couldn't send reserved bytes.")
+	}
+
+	infoHash := seeder.MetaInfo.GetInfoHash()
+	n, err = seeder.SeederWriter.Write(infoHash)
+	if err != nil {
+		return err
+	}
+
+	if n < len(infoHash) {
+		return errors.New("Couldn't send infohash bytes.")
+	}
+
+	n, err = seeder.SeederWriter.Write(seeder.SeederInfo.PeerId)
+	if err != nil {
+		return err
+	}
+
+	if n < len(seeder.SeederInfo.PeerId) {
+		return errors.New("Couldn't send peer id bytes.")
+	}
+
+	// If seeder agrees, it won't severe the connection.
+	return nil
+}
+
+func Send(writer io.Writer, message *PeerMessage) error {
+	if message.Type == KeepAlive {
+		_, err := writer.Write([]byte{})
+		return err
+	}
+
+	typeByte := []byte{message.Type}
+	buffer := bytes.NewBuffer(typeByte)
+
+	// Todo
+	// Make this a little bit better :)
+
+	switch payload := message.Payload.(type) {
+	case RequestPayload:
+		binary.Write(buffer, binary.BigEndian, payload)
+	case HavePayload:
+		binary.Write(buffer, binary.BigEndian, payload)
+	case PiecePayload:
+		binary.Write(buffer, binary.BigEndian, payload.Index)
+		binary.Write(buffer, binary.BigEndian, payload.Begin)
+		binary.Write(buffer, binary.BigEndian, payload.Piece)
+	case BitfieldPayload:
+		binary.Write(buffer, binary.BigEndian, payload.Bitfield)
+	case CancelPayload:
+		binary.Write(buffer, binary.BigEndian, payload)
+	}
+
+	bytesToSend := buffer.Bytes()
+
+	n, err := writer.Write(bytesToSend)
+	if err != nil {
+		return err
+	}
+
+	if n < len(bytesToSend) {
+		errMsg := fmt.Sprintf("Couldn't send %#v", message)
+		return errors.New(errMsg)
+	}
+
+	return nil
+}
+
+func Receive(reader io.Reader, allocator func(msgType byte) []byte) (*PeerMessage, error) {
+	typeByte := make([]byte, 1)
+
+	n, err := reader.Read(typeByte)
+	if err != nil {
+		return nil, err
+	}
+
+	if n == 0 {
+		return &KeepAliveMessage, nil
+	}
+
+	var payload any
+
+	// TODO
+	// Write this better
+
+	switch typeByte[0] {
+	case Choke:
+		return &ChokeMessage, nil
+	case Unchoke:
+		return &UnchokeMessage, nil
+	case Interested:
+		return &InterestedMessage, nil
+	case NotInterested:
+		return &NotInterestedMessage, nil
+	case Have:
+		havePayload := HavePayload{}
+		binary.Read(reader, binary.BigEndian, &havePayload)
+		payload = havePayload
+		break
+	case Request:
+		rqPayload := RequestPayload{}
+		binary.Read(reader, binary.BigEndian, &rqPayload)
+		payload = rqPayload
+		break
+	case Cancel:
+		cancPayload := CancelPayload{}
+		binary.Read(reader, binary.BigEndian, &cancPayload)
+		payload = cancPayload
+		break
+	case Bitfield:
+		bfPayload := BitfieldPayload{Bitfield: allocator(typeByte[0])}
+		binary.Read(reader, binary.BigEndian, &bfPayload.Bitfield)
+		payload = bfPayload
+		break
+	case Piece:
+		piecePayload := PiecePayload{Piece: allocator(typeByte[0])}
+		binary.Read(reader, binary.BigEndian, &piecePayload.Index)
+		binary.Read(reader, binary.BigEndian, &piecePayload.Begin)
+		binary.Read(reader, binary.BigEndian, &piecePayload.Piece)
+
+		payload = piecePayload
+		break
+	}
+
+	peerMsg := PeerMessage{Type: typeByte[0], Payload: payload}
+
+	return &peerMsg, nil
 }
---
+++ torrent/peer_test.go
@@ -0,0 +1,137 @@
+package torrent
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestInitiatingHandshake(t *testing.T) {
+	writeBuffer := bytes.NewBuffer([]byte{})
+
+	seeder := Seeder{SeederInfo: PeerInfo{PeerId: []byte(randSeq(20))}, SeederWriter: writeBuffer, MetaInfo: &MetaInfo{infoHash: []byte(randSeq(20))}}
+
+	err := seeder.InitiateHandshake()
+
+	if err != nil {
+		t.Errorf("Did not expect error %#v", err)
+	}
+
+	expectingSequence := []struct {
+		name  string
+		bytes []byte
+	}{
+		{"Protocol", []byte(HandshakeMsg)},
+		{"Reserved", make([]byte, 8)},
+		{"Infohash", seeder.MetaInfo.GetInfoHash()},
+		{"PeerId", seeder.SeederInfo.PeerId},
+	}
+
+	for i := range expectingSequence {
+		readingBytes := make([]byte, len(expectingSequence[i].bytes))
+
+		n, err := writeBuffer.Read(readingBytes)
+		if err != nil {
+			t.Errorf("Did not expect err %#v", err)
+		}
+
+		if n < len(readingBytes) {
+			t.Errorf("Could not read %d bytes", len(readingBytes))
+		}
+
+		if err == nil {
+			if !reflect.DeepEqual(readingBytes, expectingSequence[i].bytes) {
+				t.Errorf("Expected %#v, got %#v", expectingSequence[i].bytes, readingBytes)
+			}
+		}
+	}
+}
+
+type peerMsgSendTestCase struct {
+	name        string
+	msg         PeerMessage
+	sentBytes   []byte
+	wantedError error
+}
+
+func TestSend(t *testing.T) {
+	testCases := []peerMsgSendTestCase{
+		{"Choke send", ChokeMessage, []byte{Choke}, nil},
+		{"Unchoke send", UnchokeMessage, []byte{Unchoke}, nil},
+		{"Interested send", InterestedMessage, []byte{Interested}, nil},
+		{"Not interested send", NotInterestedMessage, []byte{NotInterested}, nil},
+		{"Keepalive send", KeepAliveMessage, []byte{}, nil},
+		{"Bitfield send", PeerMessage{Type: Bitfield, Payload: BitfieldPayload{Bitfield: []byte{1, 2, 3, 4}}}, []byte{5, 1, 2, 3, 4}, nil},
+		{"Request send", PeerMessage{Type: Request, Payload: RequestPayload{Index: 0, Begin: 1, Length: 5}}, []byte{6, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5}, nil},
+		{"Piece send", PeerMessage{Type: Piece, Payload: PiecePayload{Index: 0, Begin: 0, Piece: []byte{1, 2, 3, 4, 5}}}, []byte{7, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5}, nil},
+		{"Cancel send", PeerMessage{Type: Cancel, Payload: CancelPayload{Index: 0, Begin: 1, Length: 5}}, []byte{8, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5}, nil},
+	}
+
+	for i := range testCases {
+		buffer := bytes.NewBuffer([]byte{})
+
+		err := Send(buffer, &testCases[i].msg)
+
+		if err == nil {
+			if !reflect.DeepEqual(testCases[i].sentBytes, buffer.Bytes()) {
+				t.Errorf("%s wanted to send %#v, but sent %#v", testCases[i].name, testCases[i].sentBytes, buffer.Bytes())
+			}
+		}
+
+		if err != nil {
+			if err != testCases[i].wantedError {
+				t.Errorf("%s wanted error %#v, but got %#v", testCases[i].name, testCases[i].wantedError, err)
+			}
+		}
+	}
+}
+
+type peerMsgReceiveTestCase struct {
+	name           string
+	bytesToReceive []byte
+	peerMsg        PeerMessage
+	wantedError    error
+}
+
+func TestReceive(t *testing.T) {
+	testCases := []peerMsgReceiveTestCase{
+		{"Choke recv", []byte{Choke}, ChokeMessage, nil},
+		{"Unchoke recv", []byte{Unchoke}, UnchokeMessage, nil},
+		{"Interested recv", []byte{Interested}, InterestedMessage, nil},
+		{"Not interested recv", []byte{NotInterested}, NotInterestedMessage, nil},
+		// {"Keepalive recv", []byte{}, KeepAliveMessage, nil},
+		{"Bitfield recv", []byte{5, 1, 2, 3, 4}, PeerMessage{Type: Bitfield, Payload: BitfieldPayload{Bitfield: []byte{1, 2, 3, 4}}}, nil},
+		{"Request recv", []byte{6, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5}, PeerMessage{Type: Request, Payload: RequestPayload{Index: 0, Begin: 1, Length: 5}}, nil},
+		{"Piece recv", []byte{7, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5}, PeerMessage{Type: Piece, Payload: PiecePayload{Index: 0, Begin: 1, Piece: []byte{1, 2, 3, 4, 5}}}, nil},
+		{"Cancel recv", []byte{8, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5}, PeerMessage{Type: Cancel, Payload: CancelPayload{Index: 0, Begin: 1, Length: 5}}, nil},
+	}
+
+	for i := range testCases {
+		writer := bytes.NewBuffer([]byte{})
+
+		writer.Write(testCases[i].bytesToReceive)
+
+		msg, err := Receive(writer, func(msgType byte) []byte {
+			switch msgType {
+			case Bitfield:
+				return make([]byte, 4)
+			case Piece:
+				return make([]byte, 5)
+			default:
+				return nil
+			}
+		})
+
+		if err == nil {
+			if !reflect.DeepEqual(testCases[i].peerMsg, *msg) {
+				t.Errorf("%s wanted to recv %#v, but recv %#v", testCases[i].name, testCases[i].peerMsg, msg)
+			}
+		}
+
+		if err != nil {
+			if err != testCases[i].wantedError {
+				t.Errorf("%s wanted error %#v, but got %#v", testCases[i].name, testCases[i].wantedError, err)
+			}
+		}
+	}
+}
